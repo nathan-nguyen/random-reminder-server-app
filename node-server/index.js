b28f2b20556efe1a6d9de902761a04b4c3ee862b
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-05-12 14:25:21 +0800

    Initial commit

---
+++ node-server/index.js
@@ -0,0 +1,7 @@
+const app = express();
+
+const PORT = 3001;
+
+app.listen(PORT);
+console.log('\n------------------------------------------ Server Up ------------------------------------------\n');
+
