1660af5606f0f02446b93124aa5e22de1f92f887
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-05-15 01:54:42 +0800

    Added tag

--- node-server/db/db.js
+++ node-server/db/db.js
@@ -3,6 +3,7 @@ module.exports = function (){
 	var db = {};
 	var Nedb = require('nedb');
 	var post = new Nedb({ filename: './db/post.db', autoload: true });
+	var tag = new Nedb({ filename: './db/tag.db', autoload: true });
 
 	db.queryPost = function(id, cb) {
 		post.find({ _id: id }, function (err, docs) {
@@ -20,6 +21,23 @@ module.exports = function (){
 			return cb(err, docs);
 		});
 	}
+
+	db.queryTag = function(id, cb) {
+                tag.find({ _id: id }, function (err, docs) {
+                        if (err) {
+                                console.log(err);
+                                return cb(err);
+                        }
+
+                        if (docs.length > 0) {
+                                console.log("Content for tag id " + id + ": " + docs[0].content);
+                        }
+                        else {
+                                console.log("Content for tag id " + id + " does not exist in db");
+                        }
+                        return cb(err, docs);
+                });
+        }
 
 	return db;
 }
--- node-server/routes/api.js
+++ node-server/routes/api.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-//router.use(bodyParser.json());
+router.use(bodyParser.json());
 
 var winston = require('winston');                                                               //logger module
 var logger = new (winston.Logger)({
@@ -33,6 +33,30 @@ router.route('/post/:_id').get(function (req, res) {
 			}
 		}
 	});
+});
+
+router.route('/tag/:_id').get(function (req, res) {
+        db.queryTag(req.params._id, function(err, docs) {
+                if (err){
+                        logger.error(err);
+                        res.sendFile(path.join(__dirname + '/../html/500.html'));
+                }   
+                else {
+                        if (docs.length > 0) {
+				console.log(docs[0].content);
+				var s = "<html><body><h3>" + req.params._id + "</h3>";
+				for (var i = 0; i < docs[0].content.length; i++) {
+					s += '<a href="/post/' + docs[0].content[i] + '">' + docs[0].content[i] + '</a><br/>';
+				}
+				s += "</body></html>";
+				
+				res.send(s);
+                        }   
+                        else {
+                                res.sendFile(path.join(__dirname + '/../html/404.html'));
+                        }   
+                }   
+        }); 
 });
 
 module.exports = router;
